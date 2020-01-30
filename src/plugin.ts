bc31d9afe36755d0e1047aa0a9cc1ff03ddda1d7
Andreas Huber Dönni <andreashuber69@users.noreply.github.com>
2020-01-30 21:18:26 +0100

    fix: tslint

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,7 +1,10 @@
+// tslint:disable:file-name-casing
 import { Compiler } from "webpack";
 
+// tslint:disable-next-line: no-default-export
 export default class AsyncCssPlugin {
     public apply(compiler: Compiler): void {
         console.log(compiler);
+        console.log(this);
     }
 }
