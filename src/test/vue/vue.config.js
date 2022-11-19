134f5a4164c2b8f58e4a6fc9b988fb59b7d80014
Andreas Huber <andreashuber69@users.noreply.github.com>
2022-11-19 08:37:18 +0100

    docs: add comment

--- src/test/vue/vue.config.js
+++ src/test/vue/vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
     configureWebpack: config => {
         config.entry.app = "./main.js";
     },
+    // Added for async css loading
     chainWebpack: config => {
         config.plugin("html-webpack-plugin").use(HtmlWebpackPlugin);
         config.plugin("async-css-plugin").use(AsyncCssPlugin, [{ logLevel: "info" }]);
