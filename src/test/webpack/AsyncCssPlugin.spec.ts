951ef3a6b2c04b63a4d73bded008cc92246cf75e
Andreas Huber <andreashuber69@users.noreply.github.com>
2022-11-20 11:58:31 +0100

    refactor: remove superfluous optional chaining

--- src/test/webpack/AsyncCssPlugin.spec.ts
+++ src/test/webpack/AsyncCssPlugin.spec.ts
@@ -13,8 +13,8 @@ import standardOptions from "./standard.config";
 const createMochaFunc = (options: Configuration, expectedMedia: string): Mocha.Func =>
     (done) => webpack(options, (err, stats) => {
         expect(Boolean(err)).to.equal(false);
-        expect(stats?.hasErrors()).to.equal(false);
-        const outputPath = stats?.toJson().outputPath ?? "";
+        expect(stats.hasErrors()).to.equal(false);
+        const outputPath = stats.toJson().outputPath ?? "";
         expect(Boolean(outputPath)).to.equal(true);
         const { href, media } = getLinkProperties(join(outputPath, "index.html"));
         expect(href).to.equal("main.css");
