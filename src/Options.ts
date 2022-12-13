509ef0196015c2674692f94f1edc6282365c1f0e
Andreas Huber <andreashuber69@users.noreply.github.com>
2022-12-13 17:54:17 +0100

    chore: remove superfluous eslint-disable

--- src/Options.ts
+++ src/Options.ts
@@ -1,6 +1,5 @@
 export type MessageType = "error" | "info" | "warn";
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
 export interface Options {
     /** "info" logs everything, "warn" logs warnings and errors, "error" logs errors only. Default is "warn". */
     readonly logLevel?: MessageType;
