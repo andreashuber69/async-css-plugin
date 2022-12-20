f14d0dac03cdcd376bbcab6d1c4d1202a7eb9654
Andreas Huber <andreashuber69@users.noreply.github.com>
2022-12-20 06:34:34 +0100

    chore: update eslint-config and remove peers

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,8 @@
 // https://github.com/andreashuber69/async-css-plugin#--
 module.exports = {
     extends: "@andreashuber69",
+    env: {
+        browser: true,
+    },
     ignorePatterns: ["/**/*.config.js"],
 };
