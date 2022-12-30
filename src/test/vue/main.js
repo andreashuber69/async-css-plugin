41e2646a3d79d33097c5a73a1b34110d8114cc14
Andreas Huber <andreashuber69@users.noreply.github.com>
2022-12-30 18:25:37 +0100

    feat: add --report-unused-disable-directives

--- src/test/vue/main.js
+++ src/test/vue/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-// eslint-disable-next-line max-len
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 createApp(App).mount("#app");
