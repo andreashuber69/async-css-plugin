95738fecec0f2863d704d77c8bc3dee59dbb0835
Andreas Huber <andreashuber69@users.noreply.github.com>
2024-03-28 19:23:42 +0100

    chore: update packages

--- src/test/AsyncCssPlugin.spec.ts
+++ src/test/AsyncCssPlugin.spec.ts
@@ -6,7 +6,7 @@ import type { Compilation } from "webpack";
 // We need to use plain old require here, so that the compiled output is only loaded during the test run. Otherwise, tsc
 // will complain that it cannot overwrite an input file.
 // eslint-disable-next-line @stylistic/max-len
-// eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment, import/no-commonjs, @typescript-eslint/no-require-imports
+// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment, import/no-commonjs, @typescript-eslint/no-require-imports
 const AsyncCssPlugin = require("../..");
 
 const createFakeCompiler = () => {
