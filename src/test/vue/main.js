88cb83e2345be31bfd1ce621615085fccb26b879
Andreas Huber <andreashuber69@users.noreply.github.com>
2025-01-18 09:34:15 +0100

    chore: remove superfluous eslint-disable

--- src/test/vue/main.js
+++ src/test/vue/main.js
@@ -3,5 +3,4 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 createApp(App).mount("#app");
--- src/test/vue/vue.config.js
+++ src/test/vue/vue.config.js
@@ -1,6 +1,5 @@
 // https://github.com/andreashuber69/async-css-plugin/blob/develop/README.md#----async-css-plugin
 
-// eslint-disable-next-line n/no-deprecated-api
 if (!require.extensions[".ts"]) {
     require("ts-node").register({
         project: "./tsconfig.json",
